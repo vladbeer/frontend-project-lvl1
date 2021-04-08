ea1b5871471af41d252dd4334dfea833953e5304
Vladimir Zjazin <vladbeer@bk.ru>
2021-04-08 17:12:02 +0300

    brain-progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+import {name} from '../src/cli.js'
+import readlineSync from 'readline-sync'
+import {randomNunber} from '../src/index.js'
+
+console.log('Welcome to the Brain Games!')
+
+const userName = name()
+
+console.log(`Hello, ${userName}!`);
+
+console.log('What number is missing in the progression?');
+
+const isProgression = () => {
+
+    for (let i = 0; i < 3; i++) {
+
+        const index = randomNunber(0, 5);
+        const pass = randomNunber(0, 9);
+        let firstNumber = randomNunber(1, 50);
+        const arr = [];
+
+        for (let j = 0; j < 10; j++) {
+            arr.push(firstNumber);
+            firstNumber += index;
+        }
+
+        const answerCorrect = arr[pass];
+        arr[pass] = '..';
+
+        console.log(`Question: ${arr}`);
+
+        const answer = readlineSync.question('Your answer: ');
+
+        if (answerCorrect === +answer) {
+            console.log('Correct!');
+        } else {
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${answerCorrect}.\nLet's try again, ${userName}`);
+            return;
+        }
+
+    }
+
+    console.log(`Congratulations, ${userName}!`);
+
+}
+
+isProgression();
